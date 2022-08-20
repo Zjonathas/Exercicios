1311847c186134f7665b8ef45eef21027723523e
Jonathas <jonathas92244@gmail.com>
2022-08-20 08:10:17 -0300

    adiciona o número da linha

---
+++ numerando_linhas.py
@@ -0,0 +1,15 @@
+"""Faça um programa que abre um arquivo de texto qualquer (crie um pra fazer o teste) e
+acrescente no início de cada linha uma numeração. O programa deve exibir o conteúdo
+com as linhas adicionadas como no exemplo abaixo:
+001: texto qualquer
+002: qualquer texto
+003: outro texto aleatório qualquer"""
+
+abrir = open('qualquer.txt', 'x')
+abrir.close()
+
+with open('qualquer.txt', 'r+', encoding='utf_8') as p:
+    contador = 0
+    for linha in p:
+        contador += 1
+        print(contador, linha, end='')
