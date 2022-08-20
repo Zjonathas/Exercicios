00668ada7cdf413d34608e027addb16cdc4fcd1a
Jonathas <jonathas92244@gmail.com>
2022-08-20 07:56:05 -0300

    melhorado

--- coverter_valor_em_moeda.py
+++ coverter_valor_em_moeda.py
@@ -5,9 +5,12 @@ de dez centavos, uma de 5 centavos e três de um centavo."""
 
 
 def converter(valor):
+    resultao_parcial = ''
+    resultao_parcial2 = ''
     valores = [1, 0.50, 0.25, 0.10, 0.05, 0.01]
     resultado = 0
     lista = []
+    imprimir = []
     for p in valores:
         while True:
             resultado += p
@@ -15,9 +18,21 @@ def converter(valor):
             if resultado > valor:
                 resultado -= p
                 lista.pop()
+                teste = p
+                if teste > 0.50:
+                    resultao_parcial = f'{len(lista)} moeda(s) de {teste} real\n'
+                    imprimir.append(resultao_parcial)
+                    lista = []
+                else:
+                    if len(lista) == 0:
+                        break
+                    else:
+                        resultao_parcial2 = f'{len(lista)} moeda(s) de {teste} centavos\n'
+                        imprimir.append(resultao_parcial2)
+                        lista = []
                 break
 
-    print(f'O dinheiro ficou assim {lista}')
+    print(f'Você precisará de: \n {" ".join(imprimir)}')
 
 
 converter(3.78)
